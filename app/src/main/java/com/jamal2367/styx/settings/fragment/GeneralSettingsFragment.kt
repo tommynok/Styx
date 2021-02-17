d0640268c783b0fcb0a3e7d70b2562f1045d1934
Jamal <jamal2362@googlemail.com>
2021-02-17 13:54:36 +0100

    Sort Suggestion Dialog and fix none suggestion doesn't work.

--- app/src/main/java/com/jamal2367/styx/settings/fragment/GeneralSettingsFragment.kt
+++ app/src/main/java/com/jamal2367/styx/settings/fragment/GeneralSettingsFragment.kt
@@ -467,8 +467,8 @@ class GeneralSettingsFragment : AbstractSettingsFragment() {
             Suggestions.NONE -> getString(R.string.search_suggestions_off)
             Suggestions.GOOGLE -> getString(R.string.powered_by_google)
             Suggestions.DUCK -> getString(R.string.powered_by_duck)
-            Suggestions.BAIDU -> getString(R.string.powered_by_baidu)
             Suggestions.NAVER -> getString(R.string.powered_by_naver)
+            Suggestions.BAIDU -> getString(R.string.powered_by_baidu)
         }
 
     private fun showSearchSuggestionsDialog(summaryUpdater: SummaryUpdater) {
@@ -479,17 +479,17 @@ class GeneralSettingsFragment : AbstractSettingsFragment() {
             val currentChoice = when (Suggestions.from(userPreferences.searchSuggestionChoice)) {
                 Suggestions.GOOGLE -> 0
                 Suggestions.DUCK -> 1
-                Suggestions.BAIDU -> 2
-                Suggestions.NAVER -> 3
-                Suggestions.NONE -> 3
+                Suggestions.NAVER -> 2
+                Suggestions.BAIDU -> 3
+                Suggestions.NONE -> 4
             }
 
             setSingleChoiceItems(R.array.suggestions, currentChoice) { _, which ->
                 val suggestionsProvider = when (which) {
                     0 -> Suggestions.GOOGLE
                     1 -> Suggestions.DUCK
-                    2 -> Suggestions.BAIDU
-                    3 -> Suggestions.NAVER
+                    2 -> Suggestions.NAVER
+                    3 -> Suggestions.BAIDU
                     4 -> Suggestions.NONE
                     else -> Suggestions.GOOGLE
                 }
