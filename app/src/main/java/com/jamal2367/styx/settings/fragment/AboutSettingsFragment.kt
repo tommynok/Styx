0d96084f12c79229cdab8e867c6efb49a6e085ec
Jamal <jamal2362@googlemail.com>
2021-03-17 11:34:22 +0100

    Added WebView version to about settings.

--- app/src/main/java/com/jamal2367/styx/settings/fragment/AboutSettingsFragment.kt
+++ app/src/main/java/com/jamal2367/styx/settings/fragment/AboutSettingsFragment.kt
@@ -6,6 +6,9 @@ package com.jamal2367.styx.settings.fragment
 import com.jamal2367.styx.BuildConfig
 import com.jamal2367.styx.R
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.webkit.WebViewCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 
 class AboutSettingsFragment : AbstractSettingsFragment() {
@@ -15,15 +18,30 @@ class AboutSettingsFragment : AbstractSettingsFragment() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         super.onCreatePreferences(savedInstanceState, rootKey)
 
+        var webview = resources.getString(R.string.unknown)
+
+        context?.let {
+            WebViewCompat.getCurrentWebViewPackage(it)?.versionName?.let {
+                webview = it
+            }
+        }
+
         clickablePreference(
             preference = SETTINGS_VERSION,
-            summary = BuildConfig.VERSION_NAME,
+            summary = BuildConfig.VERSION_NAME
+        )
+
+        clickablePreference(
+            preference = WEBVIEW_VERSION,
+            summary = webview,
+            onClick = { }
         )
 
     }
 
     companion object {
         private const val SETTINGS_VERSION = "pref_version"
+        private const val WEBVIEW_VERSION = "pref_webview"
         private const val TAG = "AboutSettingsFragment"
     }
 }
